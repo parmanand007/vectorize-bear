8c306b84e966bd587927a559fcbd69e005a36ad8
parmanand007 <parmanandprajapati006@gmail.com>
2024-02-24 22:21:14 +0530

    changes in lib for test

--- src/app/vectorizeImage/@upload/page.jsx
+++ src/app/vectorizeImage/@upload/page.jsx
@@ -1,29 +1,64 @@
 "use client";
-import React, { useState } from "react";
-import { FileUploader } from "react-drag-drop-files";
+// import React, { useState } from "react";
+// import { FileUploader } from "react-drag-drop-files";
 import UploadDisplay from "../../../components/UploadDisplay";
-import UploadedDocuments from "../../../components/UploadedDocuments";
-const fileTypes = ["JPG", "PNG", "JPEG"];
+// import UploadedDocuments from "../../../components/UploadedDocuments";
+// const fileTypes = ["JPG", "PNG", "JPEG"];
 
-function Upload() {
-  const [file, setFile] = useState(null);
-  const handleChange = (file) => {
-    setFile(file);
-  };
+// function Upload() {
+//   const [file, setFile] = useState(null);
+//   const handleChange = (file) => {
+//     setFile(file);
+//   };
+
+//   return (
+//     <div className=" flex justify-center items-center   h-[100vh] ">
+//       <div className="w-1/3 h-1/3 flex justify-center items-center rounded-lg border-dashed border-4 border-solid-blue-500 bg-gray-100">
+//         <FileUploader
+//           handleChange={handleChange}
+//           name="file"
+//           types={fileTypes}
+//           children={!file ? <UploadDisplay  />:<UploadedDocuments file={file} />}
+//           maxSize={4}
+//           multiple={true}
+          
+
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Upload;
+
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
-    <div className=" flex justify-center items-center   h-[100vh] ">
-      <div className="w-1/3 h-1/3 flex justify-center items-center rounded-lg border-dashed border-4 border-solid-blue-500 bg-gray-100">
-        <FileUploader
-          handleChange={handleChange}
-          name="file"
-          types={fileTypes}
-          children={!file ? <UploadDisplay  />:<UploadedDocuments file={file} />}
-          maxSize={4}
-        />
-      </div>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        !isDragActive ?
+
+        <div className=" flex justify-center items-center   h-[100vh] " >
+          <div className="w-1/3 h-1/3 flex justify-center items-center rounded-lg border-dashed border-4 border-solid-blue-500 bg-gray-100">
+            
+            
+            <UploadDisplay />
+            </div>
+
+        </div>:""
+          // <p>Drop the files here ...</p> :
+          // <p>Drag 'n' drop some files here, or click to select files</p>
+      }
     </div>
-  );
+  )
 }
 
-export default Upload;
+export default MyDropzone
--- src/components/UploadedDocuments.jsx
+++ src/components/UploadedDocuments.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const UploadedDocuments = ({file}) => {
+  console.log("file==== uploaded documents",file)
   return (
     <div>UploadedDocuments</div>
   )
